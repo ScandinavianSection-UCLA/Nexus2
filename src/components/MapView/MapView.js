d76271025810431b37881dd42b31864a5c95a5dc
dhuangg <danielhuang@g.ucla.edu>
2018-04-07 16:08:37 -0400

    added mapview template

---
+++ src/components/MapView/MapView.js
@@ -0,0 +1,22 @@
+/**
+ * Created by danielhuang on 4/7/18.
+ */
+import React, { Component } from 'react';
+
+class MapView extends Component {
+
+    constructor(){
+        super();
+
+    }
+
+    render() {
+        return (
+            <div className="MapView grid-x">
+
+            </div>
+        );
+    }
+}
+
+export default MapView;
