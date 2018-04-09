187b5d59da968536986d7a6207a934f4427d4786
dhuangg <danielhuang@g.ucla.edu>
2018-04-08 21:32:35 -0400

    added location data to MapView, added kitty loading screen lol.

--- src/components/MapView/MapView.js
+++ src/components/MapView/MapView.js
@@ -3,6 +3,7 @@
  */
 import React, { Component } from 'react';
 import './MapView.css'
+/*Import leaflet here*/
 
 class MapView extends Component {
 
@@ -13,10 +14,22 @@ class MapView extends Component {
 
     /*Insert Functions Here*/
 
+    /*
+    * An array of places will be dynamically given to the mapping component depending on which view is visible
+    * (e.g. home tab will provide a list of places of whatever ontology a user clicks on, story view will provide
+     * a list of places related to the story).
+    * To access the list of places use "this.props.places" (console.log example below)
+    * In the case that the user clicks on a fieldtrip or a list of fieldtrip...
+    *  ...=> you can access it through "this.props.fieldtrips" (console.log example below)
+    * */
+
     render() {
+        /*Insert initialization functions here*/
+        console.log(this.props.places);
+        console.log(this.props.fieldtrips);
         return (
             <div className="MapView">
-
+                {/*Insert mapping tag (i.e. <div id="map"/>) here*/}
             </div>
         );
     }
