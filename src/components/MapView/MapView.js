c97dcf6b8b672560ce1daffd4d9fb8315ce162d4
shivanshug14 <shivanshug14@gmail.com>
2018-04-22 13:10:08 -0700

    React framework
    updated, installed dependencies

--- src/components/MapView/MapView.js
+++ src/components/MapView/MapView.js
@@ -1,10 +1,18 @@
 /**
  * Created by danielhuang on 4/7/18.
  */
+
 import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Map, TileLayer } from 'react-leaflet';
 import './MapView.css';
 import {Leaflet as L} from 'leaflet';
 import ReactDOM from 'react-dom';
+
+const stamenTonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png';
+const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+const mapCenter = [39.9528, -75.1638];
+const zoomLevel = 12;
 
 class MapView extends Component {
 
@@ -28,7 +36,15 @@ class MapView extends Component {
 
         return (
             <div className="MapView" id="MapView">
-                {/*Insert mapping tag (i.e. <div id="map"/>) here*/}
+                {<Map
+                    center={mapCenter}
+                    zoom={zoomLevel}
+                >
+                    <TileLayer
+                        attribution={stamenTonerAttr}
+                        url={stamenTonerTiles}
+                    />
+                </Map>}
             </div>
         );
     }
