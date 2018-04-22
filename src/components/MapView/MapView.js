3160849ff543f49275b1aac6fdeb70f2f438d299
berosen <ben.elan.rosen@gmail.com>
2018-04-22 15:09:20 -0700

    started integration of map data into react component, added css into 'mapview.css' to change the map container

--- src/components/MapView/MapView.js
+++ src/components/MapView/MapView.js
@@ -2,48 +2,46 @@
  * Created by danielhuang on 4/7/18.
  */
 import React, { Component } from 'react';
+
 import './MapView.css';
 import {Leaflet as L} from 'leaflet';
 import ReactDOM from 'react-dom';
+import {Map, TileLayer, Marker, Popup,GeoJsonCluster,GeoJson} from 'react-leaflet-universal';
+import AJAX from 'leaflet-ajax';
+
 
 class MapView extends Component {
-
-    constructor(props) {
-        super(props);
-
-
-        //*****
-        /*Insert Functions Here*/
-renderMap(){
-
+    state = {
+        lat: 56.2639,
+        lng: 9.5018,
+        zoom: 8,
     }
+    //const xx=3;
+     //var placesGeoData= new L.GeoJSON.AJAX("./places_geo.geojson")
+    render(){
+        const position = [this.state.lat, this.state.lng]
+        return (<div className="MapView" id="MapView">
+            <Map center={position} zoom={this.state.zoom}>
 
-enabled() {
-document.write("<img border=\"0\" src=\"http://4.bp.blogspot.com/-C4vvMEB9MyI/TfW0lduV2NI/AAAAAAAAAZc/N7HL1pUusGw/s1600/some%20image.png\" />");
-}
-
-
-
-
-    /*
-    * An array of places will be dynamically given to the mapping component depending on which view is visible
-    * (e.g. home tab will provide a list of places of whatever ontology a user clicks on, story view will provide
-     * a list of places related to the story).
-    * To access the list of places use "this.props.places" (console.log example below)
-    * In the case that the user clicks on a fieldtrip or a list of fieldtrip...
-    *  ...=> you can access it through "this.props.fieldtrips" (console.log example below)
-    * */
-
-    render() {
-        /*Insert initialization functions here*/
-        console.log(this.props.places);
-        console.log(this.props.fieldtrips);
-        return (
-            <div className="MapView">
-                {/*Insert mapping tag (i.e. <div id="map"/>) here*/}
+                <TileLayer
+                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                
+                <Marker position={position}>
+                    <Popup>
+            <span>
+              Popup here <br /> Can edit this pretty easily.
+            </span>
+                    </Popup>
+                </Marker>
+            </Map>
             </div>
-        );
+        )
     }
 }
+
+
+
 
 export default MapView;
