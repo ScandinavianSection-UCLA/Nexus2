d1ce95c61bd1482edd7fe799d60b6b404e2bd65a
dhuangg <danielhuang@g.ucla.edu>
2018-04-23 13:22:41 -0400

    Revert "merged with master, map displays"
    This reverts commit 0fc583e0670ecb110c8318e93a9eb8be8a203b8b.
    
    # Conflicts:
    #	.idea/workspace.xml
    #	src/components/MapView/MapView.js

--- src/components/MapView/MapView.js
+++ src/components/MapView/MapView.js
@@ -1,55 +1,47 @@
 /**
  * Created by danielhuang on 4/7/18.
  */
-
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { Map, TileLayer } from 'react-leaflet';
+
 import './MapView.css';
-import { ImageOverlay } from 'react-leaflet';
 import {Leaflet as L} from 'leaflet';
 import ReactDOM from 'react-dom';
+import {Map, TileLayer, Marker, Popup,GeoJsonCluster,GeoJson} from 'react-leaflet-universal';
+import AJAX from 'leaflet-ajax';
 
-const stamenTonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png';
-const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
-const mapCenter = [39.9528, -75.1638];
-const zoomLevel = 12;
 
 class MapView extends Component {
-
-    constructor(props) {
-        super(props);
+    state = {
+        lat: 56.2639,
+        lng: 9.5018,
+        zoom: 8,
     }
+    //const xx=3;
+     //var placesGeoData= new L.GeoJSON.AJAX("./places_geo.geojson")
+    render(){
+        const position = [this.state.lat, this.state.lng];
+        return (<div className="MapView" >
+            <Map center={position} zoom={this.state.zoom} id="MapView">
 
-    /*
-    * An array of places will be dynamically given to the mapping component depending on which view is visible
-    * (e.g. home tab will provide a list of places of whatever ontology a user clicks on, story view will provide
-     * a list of places related to the story).
-    * To access the list of places use "this.props.places" (console.log example below)
-    * In the case that the user clicks on a fieldtrip or a list of fieldtrip...
-    *  ...=> you can access it through "this.props.fieldtrips" (console.log example below)
-    * */
-
-    render() {
-        /*Insert initialization functions here*/
-        console.log(this.props.places);
-        console.log(this.props.fieldtrips);
-
-        return (
-            <div className="MapView" >
-                <Map
-                    center={mapCenter}
-                    zoom={zoomLevel}
-                    id="MapView"
-                >
-                    <TileLayer
-                        attribution={stamenTonerAttr}
-                        url={stamenTonerTiles}
-                    />
-                </Map>
+                <TileLayer
+                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                
+                <Marker position={position}>
+                    <Popup>
+            <span>
+              Popup here <br /> Can edit this pretty easily.
+            </span>
+                    </Popup>
+                </Marker>
+            </Map>
             </div>
-        );
+        )
     }
 }
+
+
+
 
 export default MapView;
