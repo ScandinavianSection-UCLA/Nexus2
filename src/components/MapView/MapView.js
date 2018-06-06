c824681b4a1e8da8e0c0a45678efb2d1f2c9c7bb
shivanshug14 <shivanshug14@gmail.com>
2018-06-05 22:24:25 -0700

    web server tiles now loading :)

--- src/components/MapView/MapView.js
+++ src/components/MapView/MapView.js
@@ -110,8 +110,9 @@ var  oldLayer = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-back
         attribution:'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
 
-var danishLayer = L.tileLayer.wms('http://kortforsyningen.kms.dk/service?servicename=topo20_hoeje_maalebordsblade&client=arcGIS&request=GetCapabilities&service=WMS&version=1.1.1&login=tango1963&password=heimskr1;',{
-    layers: 'Høje målebordsblade, 1842-1899'
+var danishLayer = L.tileLayer.wms('https://services.kortforsyningen.dk/topo20_hoeje_maalebordsblade?ignoreillegallayers=TRUE&transparent=TRUE&login=tango1963&password=heimskr1;&',{
+    layers: 'dtk_hoeje_maalebordsblade',
+    format: 'image/png'
 }).addTo(this.map);
 
 var baseMaps = {
