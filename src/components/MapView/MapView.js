030331c1ac8fb4fa1585d72bd28cfcc551a2dfc2
Neil Allavarpu <allavarpuneil@gmail.com>
2018-12-09 17:54:50 -0800

    Fixed map view for people, and when tabs are switched (+ comments!)
    Basically, a couple of `if` statements were organized improperly so that the map center and zoom level were occasionally undefined. I fixed that and rewrote/reorganized the code + commented a bunch of stuff.

--- src/components/MapView/MapView.js
+++ src/components/MapView/MapView.js
@@ -1,180 +1,199 @@
-/**
- * Created by danielhuang/benrosen on 4/7/18.
- */
+// react functionality
 import React from "react";
+// prop validation
 import PropTypes from "prop-types";
+// styling for the map
 import "./MapView.css";
+// map functionality
 import {
     LayersControl,
     Map,
     Marker,
     Popup,
-    WMSTileLayer,
     TileLayer,
-} from 'react-leaflet';
-const { BaseLayer } = LayersControl;
+    WMSTileLayer,
+} from "react-leaflet";
 
-const DEFAULT_ZOOM_LEVEL = 7;
-const DEFAULT_MAP_CENTER = [56.2639, 9.5018];
+// constants + defaults for our map
+const {BaseLayer} = LayersControl,
+    DEFAULT_MAP_CENTER = [56.2639, 9.5018],
+    DEFAULT_ZOOM_LEVEL = 7;
 
 class MapView extends React.Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {
-            tiles:[],
-            DEFAULT_MAP_CENTER: [],
-            DEFAULT_ZOOM_LEVEL: 0,
-        }
+        // start off with defaults
+        this.tiles = [{
+            "name": "Default OpenStreet Map",
+            "type": "TILE",
+            "url": "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            "attribution": "&amp;copy <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors",
+            "checked": false,
+        }, {
+            "name": "High Boards",
+            "type": "WMS",
+            "layers": "dtk_hoeje_maalebordsblade",
+            "url": "http://kortforsyningen.kms.dk/service?servicename=topo20_hoeje_maalebordsblade&client=arcGIS&request=GetCapabilities&service=WMS&version=1.1.1&login=tango1963&password=heimskr1;",
+            "format": "image/png",
+            "checked": true,
+        }, {
+            "name": "Low Boards",
+            "type": "WMS",
+            "layers": "dtk_lave_maalebordsblade",
+            "url": "http://kortforsyningen.kms.dk/service?servicename=topo20_lave_maalebordsblade&client=arcGIS&request=GetCapabilities&service=WMS&version=1.1.1&login=tango1963&password=heimskr1;",
+            "format": "image/png",
+            "checked": false,
+        }];
     }
 
-    componentWillMount(){
-        this.setState({
-            DEFAULT_MAP_CENTER: DEFAULT_MAP_CENTER,
-            DEFAULT_ZOOM_LEVEL: DEFAULT_ZOOM_LEVEL,
-            tiles:[
-                {
-                    type:'TILE',
-                    name:'Default OpenStreet Map',
-                    url:'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    attribution:'&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-                    checked:false,
-                },
-                {
-                    type:'WMS',
-                    name:'High Boards',
-                    layers:'dtk_hoeje_maalebordsblade',
-                    url:'http://kortforsyningen.kms.dk/service?servicename=topo20_hoeje_maalebordsblade&client=arcGIS&request=GetCapabilities&service=WMS&version=1.1.1&login=tango1963&password=heimskr1;',
-                    format:'image/png',
-                    checked:true,
-                },
-                {
-                    type:'WMS',
-                    name:'Low Boards',
-                    layers:'dtk_lave_maalebordsblade',
-                    url:'http://kortforsyningen.kms.dk/service?servicename=topo20_lave_maalebordsblade&client=arcGIS&request=GetCapabilities&service=WMS&version=1.1.1&login=tango1963&password=heimskr1;',
-                    format:'image/png',
-                    checked:false,
-                }
-            ]
-        });
-        console.log('map is mounting!');
+    /**
+     * Filters out invalid places from props
+     * @returns {Array<Places>} Only the valid places from props
+     */
+    prunePlaces() {
+        // for each of prop's places
+        return this.props.places.filter((place) =>
+            // make sure it isn't undefined
+            typeof place !== "undefined" &&
+            // or null
+            place !== null &&
+            // and it should have a latitude
+            place.hasOwnProperty("latitude") &&
+            // and longitude
+            place.hasOwnProperty("longitude"));
     }
 
-    defineMapCenter(){
-        var InitialPlace = this.props.places[0];
-        if(InitialPlace !== undefined) {
-            console.log('initial place is defined!', InitialPlace);
-            if ('latitude' in InitialPlace && this.props.places.length < 20) {
-                return [InitialPlace.latitude,InitialPlace.longitude]
-            }
+    /**
+     * Defines the center coordinates of the map
+     * @returns {Array<Number>} 2 element array for the coordinates of the center
+     */
+    defineMapCenter() {
+        // get filtered places
+        const places = this.prunePlaces();
+        // if places is of an adequate size
+        if (places.length > 0 && places.length < 20) {
+            // get the first place
+            const initialPlace = places[0];
+            // use that as the map center
+            return [initialPlace.latitude, initialPlace.longitude];
         } else {
-            console.log('initial place is NOT defined!');
+            // bad places, use the default
             return DEFAULT_MAP_CENTER;
         }
     }
 
-    defineMapZoom(){
-        var InitialPlace = this.props.places[0];
-        if(InitialPlace !== undefined) {
-            if ('latitude' in InitialPlace && this.props.places.length < 20) {
-                return 14;
-            }
+    /**
+     * Defines the zoom of the map
+     * @returns {Number} THe zoom of the map
+     */
+    defineMapZoom() {
+        // get filtered places
+        const places = this.prunePlaces();
+        // if places is of an adequate size
+        if (places.length > 0 && places.length < 20) {
+            // use 14 at the zoom (any reason why 14?)
+            return 14;
         } else {
+            // bad places, use the default
             return DEFAULT_ZOOM_LEVEL;
         }
     }
 
-    renderTiles(){
-        if(this.state.tiles){
-            return this.state.tiles.map((tile,i)=>{
-                switch (tile.type){
-                    case 'TILE':
-                        return <BaseLayer name={tile.name} key={i}>
+    renderTiles() {
+        return this.tiles.map((tile, i) => {
+            switch (tile.type) {
+                case "TILE":
+                    return (
+                        <BaseLayer name={tile.name} key={i}>
                             <TileLayer
                                 attribution={tile.attribution}
                                 url={tile.url}
                                 checked={tile.checked}
                             />
-                        </BaseLayer>;
-                    case 'WMS':
-                        return <BaseLayer checked name={tile.name} key={i}>
+                        </BaseLayer>
+                    );
+                case "WMS":
+                    return (
+                        <BaseLayer checked name={tile.name} key={i}>
                             <WMSTileLayer
                                 layers={tile.layers}
                                 format={tile.format}
                                 url={tile.url}
                                 checked={tile.checked}
                             />
-                        </BaseLayer>;
-                    default:
-                        return <BaseLayer name={tile.name}>
+                        </BaseLayer>
+                    );
+                default:
+                    return (
+                        <BaseLayer name={tile.name}>
                             <TileLayer
                                 attribution={tile.attribution}
                                 url={tile.url}
                                 checked={tile.checked}
                             />
-                        </BaseLayer>;
-                }
-
-            })
-        }
-    }
-
-    renderMarkers(places){
-        console.log(places);
-        var InitialPlace = places[0];
-        if(InitialPlace !== undefined){
-            if('latitude' in InitialPlace){
-                return places.map((place, i)=>{
-                    if(place !== null){
-                        let POSITION = [place.latitude, place.longitude];
-                        return <Marker position={POSITION} key={this.renderPopup(place)+i}>
-                            <Popup>
-                                {this.renderPopup(place)}
-                            </Popup>
-                        </Marker>
-                    }
-                })
+                        </BaseLayer>
+                    );
             }
-        }
+        });
     }
 
-    renderPopup(place){
-        if('full_name' in place){
-            return place['full_name'];
+    /**
+     * Generate the popups for the map
+     * @returns {Array<Marker>} Markers + popups
+     */
+    renderMarkers() {
+        // get filtered places
+        return this.prunePlaces()
+            // for each of the places
+            .map((place, i) => (
+                // create a marker and popup
+                <Marker position={[place.latitude, place.longitude]}
+                    key={i}>
+                    <Popup>
+                        {MapView.renderPopup(place)}
+                    </Popup>
+                </Marker>
+            ));
+    }
+
+    /**
+     * Render text for a place's popup
+     * @param {Object} place The place to render the pop up for
+     * @returns {String} The text for the popup
+     */
+    static renderPopup(place) {
+        // if it has a full_name property
+        if (place.hasOwnProperty("full_name")) {
+            // use that for the text
+            return place.full_name;
+            // or if it has a name property
+        } else if (place.hasOwnProperty("name")) {
+            // use that instead
+            return place.name;
         } else {
-            return place['name']
+            // bad place, warn this
+            console.warn("Place does not have full_name or name", place);
+            // don't return anything
+            return null;
         }
     }
 
     render() {
         return (
-            <Map
-                center={this.defineMapCenter.bind(this)()}
-                zoom={this.defineMapZoom.bind(this)()}
-            >
+            <Map center={this.defineMapCenter.bind(this)()}
+                zoom={this.defineMapZoom.bind(this)()}>
                 <LayersControl position="topright">
                     {this.renderTiles.bind(this)()}
-                    {this.renderMarkers.bind(this)(this.props.places)}
+                    {this.renderMarkers.bind(this)()}
                 </LayersControl>
             </Map>
-        )
+        );
     }
 }
 
-// general properties needed for this component
+// need places for the map to work
 MapView.propTypes = {
-    "person": PropTypes.array.isRequired,
     "places": PropTypes.array.isRequired,
-    "stories": PropTypes.array.isRequired,
-    "height": PropTypes.string,
-};
-
-// assign defaults if not already defined
-MapView.defaultProps = {
-    "person": [],
-    "places": [],
-    "stories": [],
 };
 
 export default MapView;
