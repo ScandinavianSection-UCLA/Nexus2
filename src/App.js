7208318ba25dd81232f4314e1361bdbeb00b2aa0
dhuangg <danielhuang@g.ucla.edu>
2018-12-24 10:31:35 -0800

    trying to see if this work pt 8 even jank solution isn't working... T.T

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     render() {
         return (
             <div className="App" >
-                <BrowserRouter basename={'/folklorenexus'}>
+                <BrowserRouter basename={'/folklorenexus/'}>
                     <Home/>
                 </BrowserRouter>
             </div>
