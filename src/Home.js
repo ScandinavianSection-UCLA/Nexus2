5a1afb73c112ef9aa523d5a13dc55a57243f2794
dhuangg <danielhuang@g.ucla.edu>
2018-12-24 10:15:43 -0800

    trying to see if this work pt 4 ....maybe

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import TabViewer from "./components/TabViewer/TabViewer";
+import Heading from "./components/Heading/Heading.js";
+
+class Home extends Component {
+    constructor() {
+        super();
+        // start in a state of loading
+        this.state = {
+            "loading": true,
+        };
+    }
+
+    componentDidMount() {
+        // simulate async action and remove loader
+        setTimeout(() => this.setState({"loading": false}), 1500);
+    }
+
+    render() {
+        return (
+            <div className="Home grid-y medium-grid-frame full">
+                {/* without nested div, the Book View gets mega-compressed */}
+                <div>
+                    {/* Top banner with flag + title on left, book icon on right */}
+                    <Heading />
+                    {/* Everything else on the page */}
+                    <TabViewer />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
